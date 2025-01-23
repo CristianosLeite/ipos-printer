8635ea613dc15b0c114c111d4943f3d10a7d7da2
Cristiano Leite <cristianosleite@hotmail.com>
2025-01-23 19:38:22 -0300

    Add return statement

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,19 +1,19 @@
 export interface IPosPrinterPlugin {
-  getPrinterStatus(): Promise<{ status: number }>;
-  getPrinterStatusMessage(options: {value: number}): Promise<{ value: string }>;
-  setPrinterPrintDepth(options: {depth: number}): Promise<void>;
-  setPrinterPrintFontType(options: {typeface: string}): Promise<void>;
-  setPrinterPrintFontSize(options: {fontSize: number}): Promise<void>;
-  setPrinterPrintAlignment(options: {alignment: number}): Promise<void>;
-  printerFeedLines(options: {lines: number}): Promise<void>;
-  printBlankLines(options: {lines: number}): Promise<void>;
-  printText(options: {text: string}): Promise<void>;
-  printSpecifiedTypeText(options: {text: string, typeface: string, fontSize: number}): Promise<void>;
-  PrintSpecFormatText(options: {text: string, typeface: string, fontSize: number, alignment: number}): Promise<void>;
-  printColumnsText(options: {colsTextArr: string[], colsWidthArr: number[], colsAlign: number[]}): Promise<void>;
-  printBitmap(options: {alignment: number, bitmapSize: number, base64: string}): Promise<void>;
-  printBarCode(options: {data: string, symbology: number, height: number, width: number, textPosition: number}): Promise<void>;
-  printQRCode(options: {data: string, moduleSize: number, errorLevel: number}): Promise<void>;
-  printRawData(options: {data: string}): Promise<void>;
-  printRowBlock(): Promise<void>;
+  getPrinterStatus(): Promise<{ result: number }>;
+  getPrinterStatusMessage(options: {value: number}): Promise<{ result: string }>;
+  setPrinterPrintDepth(options: {depth: number}): Promise<{ result: string }>;
+  setPrinterPrintFontType(options: {typeface: string}): Promise<{ result: string }>;
+  setPrinterPrintFontSize(options: {fontSize: number}): Promise<{ result: string }>;
+  setPrinterPrintAlignment(options: {alignment: number}): Promise<{ result: string }>;
+  printerFeedLines(options: {lines: number}): Promise<{ result: string }>;
+  printBlankLines(options: {lines: number}): Promise<{ result: string }>;
+  printText(options: {text: string}): Promise<{ result: string }>;
+  printSpecifiedTypeText(options: {text: string, typeface: string, fontSize: number}): Promise<{ result: string }>;
+  PrintSpecFormatText(options: {text: string, typeface: string, fontSize: number, alignment: number}): Promise<{ result: string }>;
+  printColumnsText(options: {colsTextArr: string[], colsWidthArr: number[], colsAlign: number[]}): Promise<{ result: string }>;
+  printBitmap(options: {alignment: number, bitmapSize: number, base64: string}): Promise<{ result: string }>;
+  printBarCode(options: {data: string, symbology: number, height: number, width: number, textPosition: number}): Promise<{ result: string }>;
+  printQRCode(options: {data: string, moduleSize: number, errorLevel: number}): Promise<{ result: string }>;
+  printRawData(options: {data: string}): Promise<{ result: string }>;
+  printRowBlock(): Promise<{ result: string }>;
 }
--- src/web.ts
+++ src/web.ts
@@ -3,72 +3,72 @@ import { WebPlugin } from '@capacitor/core';
 import type { IPosPrinterPlugin } from './definitions';
 
 export class IPosPrinterWeb extends WebPlugin implements IPosPrinterPlugin {
-  getPrinterStatus(): Promise<{ status: number; }> {
+  getPrinterStatus(): Promise<{ result: number; }> {
     console.log('getPrinterStatus');
-    return Promise.resolve({ status: 0 });
+    return Promise.resolve({ result: 0 });
   }
-  getPrinterStatusMessage(options: { value: number; }): Promise<{ value: string; }> {
+  getPrinterStatusMessage(options: { value: number; }): Promise<{ result: string; }> {
     console.log('getPrinterStatusMessage', options);
-    return Promise.resolve({ value: 'Success' });
+    return Promise.resolve({ result: 'Success' });
   }
-  setPrinterPrintDepth(options: { depth: number; }): Promise<void> {
+  setPrinterPrintDepth(options: { depth: number; }): Promise<{ result: string; }> {
     console.log('setPrinterPrintDepth', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  setPrinterPrintFontType(options: { typeface: string; }): Promise<void> {
+  setPrinterPrintFontType(options: { typeface: string; }): Promise<{ result: string; }> {
     console.log('setPrinterPrintFontType', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  setPrinterPrintFontSize(options: { fontSize: number; }): Promise<void> {
+  setPrinterPrintFontSize(options: { fontSize: number; }): Promise<{ result: string; }> {
     console.log('setPrinterPrintFontSize', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  setPrinterPrintAlignment(options: { alignment: number; }): Promise<void> {
+  setPrinterPrintAlignment(options: { alignment: number; }): Promise<{ result: string; }> {
     console.log('setPrinterPrintAlignment', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printerFeedLines(options: { lines: number; }): Promise<void> {
+  printerFeedLines(options: { lines: number; }): Promise<{ result: string; }> {
     console.log('printerFeedLines', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printBlankLines(options: { lines: number; }): Promise<void> {
+  printBlankLines(options: { lines: number; }): Promise<{ result: string; }> {
     console.log('printBlankLines', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printText(options: { text: string; }): Promise<void> {
+  printText(options: { text: string; }): Promise<{ result: string; }> {
     console.log('printText', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printSpecifiedTypeText(options: { text: string; typeface: string; fontSize: number; }): Promise<void> {
+  printSpecifiedTypeText(options: { text: string; typeface: string; fontSize: number; }): Promise<{ result: string; }> {
     console.log('printSpecifiedTypeText', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  PrintSpecFormatText(options: { text: string; typeface: string; fontSize: number; alignment: number; }): Promise<void> {
+  PrintSpecFormatText(options: { text: string; typeface: string; fontSize: number; alignment: number; }): Promise<{ result: string; }> {
     console.log('PrintSpecFormatText', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printColumnsText(options: { colsTextArr: string[]; colsWidthArr: number[]; colsAlign: number[]; }): Promise<void> {
+  printColumnsText(options: { colsTextArr: string[]; colsWidthArr: number[]; colsAlign: number[]; }): Promise<{ result: string; }> {
     console.log('printColumnsText', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printBitmap(options: { alignment: number; bitmapSize: number; base64: string; }): Promise<void> {
+  printBitmap(options: { alignment: number; bitmapSize: number; base64: string; }): Promise<{ result: string; }> {
     console.log('printBitmap', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printBarCode(options: { data: string; symbology: number; height: number; width: number; textPosition: number; }): Promise<void> {
+  printBarCode(options: { data: string; symbology: number; height: number; width: number; textPosition: number; }): Promise<{ result: string; }> {
     console.log('printBarCode', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printQRCode(options: { data: string; moduleSize: number; errorLevel: number; }): Promise<void> {
+  printQRCode(options: { data: string; moduleSize: number; errorLevel: number; }): Promise<{ result: string; }> {
     console.log('printQRCode', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printRawData(options: { data: string; }): Promise<void> {
+  printRawData(options: { data: string; }): Promise<{ result: string; }> {
     console.log('printRawData', options);
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
-  printRowBlock(): Promise<void> {
+  printRowBlock(): Promise<{ result: string; }> {
     console.log('printRowBlock');
-    return Promise.resolve();
+    return Promise.resolve({ result: 'Success' });
   }
 }
