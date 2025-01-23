45d85b7ccef03ab4c8850b90516299cbab1a74ae
Cristiano Leite <cristianosleite@hotmail.com>
2025-01-23 11:43:25 -0300

    removed unnecessary code

--- android/src/main/java/com/conecsa/ipos/printer/service/ThreadPoolManager.java
+++ android/src/main/java/com/conecsa/ipos/printer/service/ThreadPoolManager.java
@@ -2,7 +2,6 @@ package com.conecsa.ipos.printer.service;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class ThreadPoolManager {
   private final ExecutorService service;
@@ -21,12 +20,4 @@ public class ThreadPoolManager {
   public void executeTask(Runnable runnable) {
     service.execute(runnable);
   }
-
-  public Future<?> submitTask(Runnable task) {
-    return service.submit(task);
-  }
-
-  /**
-   * Create a task to run the command in the background.
-   */
 }
